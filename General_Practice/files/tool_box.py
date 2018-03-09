879a1945e5914cd91a88b76460da5c2fc792332d
Elad Matia <elad.matia@gmail.com>
2018-03-10 00:46:19 +0200

    IDK

--- General_Practice/files/tool_box.py
+++ General_Practice/files/tool_box.py
@@ -4,17 +4,62 @@ import re
 def main():
     path = input("Enter a file path: ")
     task = input("Enter a task: ")
+    read_line(path)
     if task == "sort":
         sort(path)
+    elif task == "rev":
+        rev(path)
+
+
+def put_in_list(path):
+    string = ""
+    new_list = []
+    no_duplicate = []
+    with open(path, "r") as text_file:
+        string = text_file.read()
+    new_list = re.sub("[^\w]", " ", string).split()  # -> taken from stack overflow! <-
+    for item in new_list:
+        if item not in no_duplicate:
+            no_duplicate.append(item)
+    return no_duplicate
+
+
+def read_line(path):
+    list = open(path).readlines()
+    new_list = []
+    index1 = 0
+    index2 = 0
+    final_string = ""
+    for line in list:
+        new_list.append(re.sub("[^\w]", " ", line).split())  # -> taken from stack overflow! <-
+    # revert all the words in the list
+    for line in new_list:
+        for word in line:
+            new_list[index1][index2] = word[::-1]
+            index2 += 1
+        index1 += 1
+        index2 = 0
+    for line in new_list:
+        for word in line:
+            final_string = final_string + " " + word
+        final_string = final_string + '\n'
+    final_string.lstrip()
+    print(final_string)
 
 
 def sort(path):
-    string = ""
-    with open(path, "r") as text_file:
-        string = text_file.read()
-    wordList = re.sub("[^\w]", " ", string).split()  # -> taken from stack overflow! <-
-    wordList.sort()
-    print(wordList)
+    word_list = put_in_list(path)
+    word_list.sort()
+    print(word_list)
+
+
+def rev(path):
+    my_list = put_in_list(path)  # get all the words separated in a list
+    new_list = []
+    for item in my_list:
+        new_list.append(item[::-1])
+    new_string = " ".join(new_list)
+    print(new_string)
 
 
 if __name__ == '__main__':
