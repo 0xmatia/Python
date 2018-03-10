75723c83d0a6d5ddebe816955aea8b28fcb406d2
Elad Matia <elad.matia@gmail.com>
2018-03-10 21:28:22 +0200

    Finished (possibly) the tool box exerciese)

--- General_Practice/files/tool_box.py
+++ General_Practice/files/tool_box.py
@@ -4,14 +4,22 @@ import re
 def main():
     path = input("Enter a file path: ")
     task = input("Enter a task: ")
-    read_line(path)
     if task == "sort":
         sort(path)
     elif task == "rev":
         rev(path)
+    elif task == "last":
+        print_line(path)
 
 
 def put_in_list(path):
+    """
+    Helper function for sort() which put the text from a given path into a file
+    :param path: The path to the file
+    :type path: str
+    :return: A list with the text file
+    :rtype: list
+    """
     string = ""
     new_list = []
     no_duplicate = []
@@ -25,6 +33,12 @@ def put_in_list(path):
 
 
 def rev(path):
+    """
+    The function prints the text file backwards
+    :param path: The path to the file
+    :type path: str
+    :return: None
+    """
     list = open(path).readlines()
     new_list = []
     another_list = []
@@ -48,24 +62,36 @@ def rev(path):
     for line in another_list:
         for word in line:
             final_string = final_string + " " + word
-        final_string = final_string + '\n'
     final_string.lstrip()
+    final_string = final_string[1:]  # removes the first space (duuno why it even creates)
     print(final_string)
 
 
+def print_line(path):
+    """
+    The function prints lines from n to the end
+    :param path: The path to the file
+    :type path: str
+    :return: None
+    """
+    n = int(input("Enter a number: "))
+    with open(path, "r") as text_file:
+        for i in enumerate(text_file):
+            if int(i[0]) >= n:
+                a = "".join(i[1:])
+                print(a)
+
+
 def sort(path):
+    """
+    The function sort the list from the file
+    :param path: The path to the file
+    :type path: str
+    :return: None
+    """
     word_list = put_in_list(path)
     word_list.sort()
     print(word_list)
-
-
-def rev(path):
-    my_list = put_in_list(path)  # get all the words separated in a list
-    new_list = []
-    for item in my_list:
-        new_list.append(item[::-1])
-    new_string = " ".join(new_list)
-    print(new_string)
 
 
 if __name__ == '__main__':
